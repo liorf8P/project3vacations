fc8da91dec3216539715a4fdd01ada4e29bbcf5f
liorf8P <74972163+liorf8P@users.noreply.github.com>
2020-11-24 17:30:49 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,24 +1,47 @@
 const mysql = require('mysql')
 
-const con = mysql.createConnection({
+
+
+
+
+var pool  = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'liorp_project3'
-})
+});
 
-con.connect((err) => {
-    if (err) {
-        console.log("error connection" + err.stack)
-        return
-    }
-    console.log('connected to my sql')
-})
+pool.getConnection(function(err, connection) {
+    connection.query( 'SELECT something FROM sometable', function(err, rows) {
+
+      console.log(pool._freeConnections.indexOf(connection)); // -1
+
+      connection.release();
+
+      console.log(pool._freeConnections.indexOf(connection)); // 0
+
+   });
+});
+
+// const con = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'liorp_project3'
+// })
+
+// con.connect((err) => {
+//     if (err) {
+//         console.log("error connection" + err.stack)
+//         return
+//     }
+//     console.log('connected to my sql')
+// })
 
 
 const myQuery = (q) => {
     return new Promise((resolve, reject) => {
-        con.query(q, (err, results) => {
+        con.pool(q, (err, results) => {
             if (err) {
                 reject(err)
             } else {
