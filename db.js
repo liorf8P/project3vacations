f927afcd4abfd6a1dac63dc43ef216787a6c54c4
liorf8P <74972163+liorf8P@users.noreply.github.com>
2020-11-24 17:37:15 +0200

    Update db.js

--- db.js
+++ db.js
@@ -4,39 +4,39 @@ const mysql = require('mysql')
 
 
 
-var pool  = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'liorp_project3'
-});
-
-pool.getConnection(function(err, connection) {
-    connection.query( 'SELECT * FROM vacations', function(err, rows) {
-
-      console.log(pool._freeConnections.indexOf(connection)); // -1
-
-      connection.release();
-
-      console.log(pool._freeConnections.indexOf(connection)); // 0
-
-   });
-});
-
-// const con = mysql.createConnection({
+// var pool  = mysql.createPool({
 //     host: 'localhost',
 //     user: 'root',
 //     password: '',
 //     database: 'liorp_project3'
-// })
+// });
 
-// con.connect((err) => {
-//     if (err) {
-//         console.log("error connection" + err.stack)
-//         return
-//     }
-//     console.log('connected to my sql')
-// })
+// pool.getConnection(function(err, connection) {
+//     connection.query( 'SELECT * FROM vacations', function(err, rows) {
+
+//       console.log(pool._freeConnections.indexOf(connection)); // -1
+
+//       connection.release();
+
+//       console.log(pool._freeConnections.indexOf(connection)); // 0
+
+//    });
+// });
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'liorp_project3'
+})
+
+con.connect((err) => {
+    if (err) {
+        console.log("error connection" + err.stack)
+        return
+    }
+    console.log('connected to my sql!@!@!@!!@!@!@!@!@!@!@!!@!@!!@!!!@!@!!@')
+})
 
 
 const myQuery = (q) => {
